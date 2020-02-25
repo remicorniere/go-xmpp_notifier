1b063847a0003b669f05a0f474c6cc025189876c
rcorniere <rcorniere@process-one.net>
2020-02-25 15:32:23 +0100

    Test secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "${INPUT_SERVER_IP}   ${INPUT_SERVER_DOMAIN}">>/etc/hosts
-go run main.go "${INPUT_SERVER_IP}"
+go run main.go "${INPUT_SERVER_IP}" "${INPUT_SERVER_DOMAIN}" "${INPUT_CORRESPONDANT}" "$INPUT_LOGIN" "${INPUT_PASS}"
--- main.go
+++ main.go
@@ -2,9 +2,51 @@ package main
 
 import (
 	"fmt"
+	"gosrc.io/xmpp"
+	"gosrc.io/xmpp/stanza"
+	"log"
 	"os"
 )
 
+const (
+	server_ip = iota
+	serverDomain
+	correspondent
+	login
+)
+
 func main() {
-	fmt.Println(os.Args[1][2:])
+	config := xmpp.Config{
+		TransportConfiguration: xmpp.TransportConfiguration{
+			Address: os.Args[serverDomain] + "5222",
+		},
+		Jid:          os.Args[login],
+		Credential:   xmpp.Password("test"),
+		StreamLogger: os.Stdout,
+		Insecure:     true,
+		// TLSConfig: tls.Config{InsecureSkipVerify: true},
+	}
+
+	router := xmpp.NewRouter()
+
+	client, err := xmpp.NewClient(config, router, errorHandler)
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	err = client.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	m := stanza.Message{Attrs: stanza.Attrs{To: os.Args[correspondent], Type: stanza.MessageTypeChat}, Body: "JUST TESTING LUL"}
+	err = client.Send(m)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func errorHandler(err error) {
+	fmt.Println(err.Error())
 }
