bce48f668044ba4b6cc783a1f9ffa3763f0d79ce
rcorniere <rcorniere@process-one.net>
2020-02-25 16:29:15 +0100

    Test secrets

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 		Insecure:     false,
 	}
 
-	fmt.Println("logging in as : " + os.Args[login])
+	fmt.Println("logging in as : " + os.Args[login][1:])
 	fmt.Println("connecting to : " + os.Args[serverDomain][1:] + ":" + port)
 	fmt.Println("sending messages to user :" + os.Args[correspondent][1:])
 	router := xmpp.NewRouter()
